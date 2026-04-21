8b851055f2b1d764a2fcfc1fc8cfc1fc589dab63
Adam Haziq <adamproml123@gmail.com>
2026-04-21 22:19:06 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+import { auth } from './firebase.js';
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+window.login = async function() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+window.register = async function() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("Registered!");
+  } catch (err) {
+    alert(err.message);
+  }
+};
